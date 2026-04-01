aa8a619d478561605deca4bc9ca689efea28c8f1
msuarz <maykel.suarez@ukg.com>
2026-04-01 15:59:31 -0400

    fix gitignore dudes/ pattern, track all dudes files

---
+++ code/lib/dude/dudes/process_manager.rb
@@ -0,0 +1,18 @@
+module Dude
+  module Dudes
+    class ProcessManager
+      def self.kill_watchers
+        pids = pgrep_all('dude abide')
+        pids.each { |pid| kill_process(pid) }
+      end
+
+      def self.pgrep_all(pattern)
+        `pgrep -f "#{pattern}"`.strip.split("\n").map(&:to_i).select(&:positive?)
+      end
+
+      def self.kill_process(pid)
+        Process.kill('TERM', pid) rescue nil
+      end
+    end
+  end
+end
