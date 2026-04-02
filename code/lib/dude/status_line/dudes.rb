5b5b09eb5c39e10004adb489ffdb85688b3d5a4b
msuarz <maykel.suarez@ukg.com>
2026-04-01 20:24:26 -0400

    Fix JSON::ParserError in Dudes initialize

--- code/lib/dude/status_line/dudes.rb
+++ code/lib/dude/status_line/dudes.rb
@@ -9,7 +9,7 @@ module Dude
       COLOR_NAMES = { 'green' => 'green', 'yellow' => 'yellow', 'red' => 'red' }
 
       def initialize(session_data, dudes_data, dude_dir, context_percentage)
-        @session = session_data.is_a?(String) ? JSON.parse(session_data) :
+        @session = session_data.is_a?(String) ? (JSON.parse(session_data) rescue {}) :
                    session_data
         @dudes = dudes_data || []
         @dude_dir = dude_dir
