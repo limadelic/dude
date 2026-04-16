6bd15f70a037433e79861821c7af13c245cb088c
msuarz <maykel.suarez@ukg.com>
2026-04-16 18:18:45 -0400

    fix line length in workflow trigger

--- code/lib/dude/dudes/alley_pr.rb
+++ code/lib/dude/dudes/alley_pr.rb
@@ -20,7 +20,8 @@ module Dude
 
       def push_and_trigger(branch)
         `git push -u origin #{branch} 2>&1`
-        `gh workflow run dude.yml --ref #{branch} -f prompt="/alley-pr"`
+        prompt = 'Create a pull request for this branch'
+        `gh workflow run dude.yml --ref #{branch} -f prompt="#{prompt}"`
       end
 
       def wait_for_workflow_completion
